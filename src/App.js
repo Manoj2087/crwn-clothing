d88cd31a03c40c13bfc3154f29c021fb7f317966
Manoj Alaghappan <manoj_alaghappan@yahoo.com>
2021-07-02 12:36:54 +1000

    added react route and route to hats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Switch ,Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import './App.css';
+
+const HatPage = () => (
+  <div>Hello Hat</div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatPage} />
+      </Switch>
     </div>
   );
 }
